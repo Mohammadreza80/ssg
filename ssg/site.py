a71e3475cc3c551761e518fbb425f7dc91c7e1ac
reza <reza.sorori@gmail.com>
2024-04-27 20:19:06 +0800

    delete test code

--- ssg/site.py
+++ ssg/site.py
@@ -10,19 +10,3 @@ class Site:
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
         return directory
-
-
-# Create instances of the Site class
-source = Path("./home/user/source")
-dest = Path("./home/user/destination")
-site = Site(source, dest)
-
-# Example usage of create_dir() method
-# Let's say we have a path "/home/user/source/dir1/file.txt"
-path = Path("./home/user/source/dir1/file.txt")
-
-# Call create_dir() method
-directory = site.create_dir(path)
-
-# Print the result
-print("Full path to destination folder:", directory)
